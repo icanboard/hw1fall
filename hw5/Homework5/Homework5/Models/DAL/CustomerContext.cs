755f1212e3b54f294e5b3312468a9b869523c4c4
jake <jthatfield15@wou.edu>
2017-11-03 23:05:43 -0700

    creating first view

---
+++ hw5/Homework5/Homework5/Models/DAL/CustomerContext.cs
@@ -0,0 +1,17 @@
+﻿using Homework5.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Homework5.DAL
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext() : base("name=MyDBContext")
+        { }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ hw5/Homework5/Homework5/Models/customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Homework5.Models
+{
+    public class Customer
+    {
+        public int CID { get; set; }
+
+        [Required]
+        public string ODL { get; set; }
+        
+        [Required]
+        public DateTime DOB { get; set; }
+
+        [Required, StringLength(64)]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required, StringLength(64)]
+        [Display(Name = "Street")]
+        public string StreetAdd { get; set; }
+
+        [Required, StringLength(24)]
+        [Display(Name = "City")]
+        public string CityAdd { get; set; }
+
+        [Required, StringLength(3)]
+        [Display(Name = "State")]
+        public string StateAdd { get; set; }
+
+        [Required, StringLength(5)]
+        [Display(Name = "Zip")]
+        public string ZipAdd { get; set; }
+
+        [Required, StringLength(2)]
+        [Display(Name = "County")]
+        public string CountyAdd { get; set; }
+        
+        public DateTime DateSigned { get; set; }
+    }
+}
