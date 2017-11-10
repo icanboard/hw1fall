e6f5b734c2f6e7b60a21db55780972a5ad942bf1
jake <jthatfield15@wou.edu>
2017-11-10 09:38:15 -0800

    initial commit for HW6

---
+++ hw6/Homework6/Homework6/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Homework6.Models; // required to add the dbcontext class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework6.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private AdventureWorksContext db = new AdventureWorksContext(); // brings in dbcontext
+        
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
