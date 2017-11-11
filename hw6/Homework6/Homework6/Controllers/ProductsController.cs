09ca11f54c1ca05c9c488ede3718ad69c114e287
jake <jthatfield15@wou.edu>
2017-11-10 17:46:07 -0800

    have product id showing when selecting a product; no error checking

--- hw6/Homework6/Homework6/Controllers/ProductsController.cs
+++ hw6/Homework6/Homework6/Controllers/ProductsController.cs
@@ -10,21 +10,53 @@ namespace Homework6.Controllers
     public class ProductsController : Controller
     {
         private AdventureWorksContext db = new AdventureWorksContext(); // brings in dbcontext
-        
+
         // GET: ~/Products/
         // GET: ~/Products/Index
+        // param id = ProductCategoryID;
         public ActionResult Index(int? id)
         {
+            // get all the productcategories (4 of them)
             var categories = db.ProductCategories;
-
+            // if there is a null, it throws an error, so check for nulls; else set to the id for use in view
             if (id != null && db.ProductCategories.Find(id) != null)
             {
                 ViewBag.catID = id;
             }
-
+            //return all the categories to iterate and disect
             return View(categories);
         }
 
-        
+        // GET: ~/Products/Products
+        // param id = ProductSubcategoryID;
+        public ActionResult Products(int? id)
+        {
+            // if theres a null, it throws an error, so check for nulls
+            if (id == null || db.ProductSubcategories.Find(id) == null)
+            {
+                // if its null, send them back to the index to pick a category
+                return RedirectToAction("Index");
+            }
+            // get all the products that match the product subcategory that was passed in
+            var products = db.ProductSubcategories.Find(id).Products.ToList();
+
+            return View(products);
+        }
+
+        // GET: ~/Products/Product
+        // param id = ProductID;
+        public ActionResult Product(int? id)
+        {
+            // if theres a null, it throws an error, so check for nulls
+            if (id == null || db.Products.Find(id) == null)
+            {
+                // if its null, send them back to the index to pick a category/item
+                return RedirectToAction("Index");
+            }
+
+            var product = db.Products.Find(id);
+
+            return View(product);
+        }
     }
 }
