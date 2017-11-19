d5d8aeaaf5725393dc27b8883cc9a0162be0c0da
jake <jthatfield15@wou.edu>
2017-11-18 21:34:43 -0800

    first commit, changes are coming

---
+++ hw7/Homework7/Homework7/Controllers/GiphyImageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework7.Controllers
+{
+    public class GiphyImageController : Controller
+    {
+        // GET: GiphyImage
+        public ActionResult Index()
+        {
+            string apiKey = System.Web.Configuration.WebConfigurationManager.AppSettings["GiphyAPIKey"];
+            Debug.WriteLine("API key = " + apiKey);
+
+            ViewBag.apiKey = apiKey; // allow Index to access apiKey
+
+            return View();
+        }
+    }
+}
